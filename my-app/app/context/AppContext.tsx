4779061b1c3c20e35340286e7bc8701a1f30d2d3
kavindiarunika <165579937+kavindiarunika@users.noreply.github.com>
2026-05-02 08:27:49 +0530

    first commit

---
+++ my-app/app/context/AppContext.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createContext, useEffect, useState, ReactNode } from "react";
+
+type AuthContextType = {
+  isLoggedIn: boolean;
+  setIsLoggedIn: (value: boolean) => void;
+  loading: boolean;
+};
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // check JWT on page load
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+
+    setLoading(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ my-app/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='w-full bg-blue-900 h-screen flex items-center justify-center'>
+      <div className='w-full m-8 p-4 border border-gray-100  '>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default page
